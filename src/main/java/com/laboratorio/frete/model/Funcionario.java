d12e2baaf6cb69ff8efa651524501e73146c30ce
Valter Pereira Oliveira <95293263+ValterOliveiraX@users.noreply.github.com>
2024-08-29 13:42:37 -0300

    func

---
+++ src/main/java/com/laboratorio/frete/model/Funcionario.java
@@ -0,0 +1,27 @@
+package com.laboratorio.frete.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+public class Funcionario {
+
+      @Id
+    private Integer matricula;
+    
+    @ManyToOne
+    @JoinColumn(name = "pessoaFisica_id")
+    private PessoaFisica pessoaFisica;
+    
+    @OneToMany(mappedBy = "funcionario")
+    private List<Dependente> dependentes;
+    
+}
