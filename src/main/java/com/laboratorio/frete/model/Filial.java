3f35517194caead7ef869e8a35628dc0fb1b8a7c
Valter Pereira Oliveira <95293263+ValterOliveiraX@users.noreply.github.com>
2024-08-29 09:23:06 -0300

    catualiz

--- src/main/java/com/laboratorio/frete/model/Filial.java
+++ src/main/java/com/laboratorio/frete/model/Filial.java
@@ -1,7 +1,17 @@
 package com.laboratorio.frete.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter @Setter
 public class Filial {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer Id;
     private String nome;
     private String endereco;
